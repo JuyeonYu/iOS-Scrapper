4feae8465b1324417dee80ba8fc81bfea6afb970
JuyeonYu <remake111@ucware.onmicrosoft.com>
2020-02-11 10:01:24 +0900

    Initial Commit

---
+++ scrapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scrapper
+//
+//  Created by 주연  유 on 2020/02/11.
+//  Copyright © 2020 johnny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
