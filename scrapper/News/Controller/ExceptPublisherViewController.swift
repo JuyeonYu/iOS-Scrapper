a61712f930cb9c9aa570783e5aa23c5b50f73aeb
juyeon <remake867@gmail.com>
2023-06-18 01:41:29 +0900

    feat: 최소 지원 버전 14로 올림

--- scrapper/News/Controller/ExceptPublisherViewController.swift
+++ scrapper/News/Controller/ExceptPublisherViewController.swift
@@ -32,35 +32,31 @@ class ExceptPublisherViewController: UIViewController {
 extension ExceptPublisherViewController: UITableViewDelegate {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     guard let cell = tableView.cellForRow(at: indexPath) else { return }
-    if #available(iOS 14.0, *) {
-      let press = Constants.press[indexPath.row]
-      var content = cell.defaultContentConfiguration()
-      content.text = press
+    let press = Constants.press[indexPath.row]
+    var content = cell.defaultContentConfiguration()
+    content.text = press
+    
+    if selectedPress.contains(press) {
+      cell.accessoryType = .none
+      selectedPress.remove(press)
       
-      if selectedPress.contains(press) {
-        cell.accessoryType = .none
-        selectedPress.remove(press)
-        
-        realm.objects(exceptNews.self).filter { $0.press == press }.forEach { press in
-          try! realm.write({
-            realm.delete(press)
-          })
-        }
-      } else {
-        cell.accessoryType = .checkmark
-        selectedPress.insert(press)
+      realm.objects(exceptNews.self).filter { $0.press == press }.forEach { press in
         try! realm.write({
-          let news = exceptNews()
-          news.press = press
-          news.domain = Constants.domain[press] ?? ""
-          realm.add(news)
+          realm.delete(press)
         })
       }
-      
-      cell.contentConfiguration = content
     } else {
-      // Fallback on earlier versions
+      cell.accessoryType = .checkmark
+      selectedPress.insert(press)
+      try! realm.write({
+        let news = exceptNews()
+        news.press = press
+        news.domain = Constants.domain[press] ?? ""
+        realm.add(news)
+      })
     }
+    
+    cell.contentConfiguration = content
   }
 }
 
@@ -68,18 +64,14 @@ extension ExceptPublisherViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
     let press = Constants.press[indexPath.row]
-    if #available(iOS 14.0, *) {
-      var content = cell.defaultContentConfiguration()
-      content.text = press
-      if selectedPress.contains(press) {
-        cell.accessoryType = .checkmark
-      } else {
-        cell.accessoryType = .none
-      }
-      cell.contentConfiguration = content
+    var content = cell.defaultContentConfiguration()
+    content.text = press
+    if selectedPress.contains(press) {
+      cell.accessoryType = .checkmark
     } else {
+      cell.accessoryType = .none
     }
-    
+    cell.contentConfiguration = content
     return cell
   }
   
@@ -89,7 +81,7 @@ extension ExceptPublisherViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     "선택한 언론사의 뉴스는 걸러냅니다."
   }
-//  func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-//    "기타 선택 시 선택되지 않는 언론사의 뉴스만 볼 수 있습니다."
-//  }
+  //  func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+  //    "기타 선택 시 선택되지 않는 언론사의 뉴스만 볼 수 있습니다."
+  //  }
 }
