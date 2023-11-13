d00f9dff73bf8a9d096dabdfef24d433d83b655f
juyeon <remake867@gmail.com>
2023-11-13 23:29:56 +0900

    chore

--- scrapper/Application/AppDelegate.swift
+++ scrapper/Application/AppDelegate.swift
@@ -27,19 +27,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     let config = Realm.Configuration(
-      // Set the new schema version. This must be greater than the previously used
-      // version (if you've never set a schema version before, the version is 0).
       schemaVersion: version,
-      
-      // Set the block which will be called automatically when opening a Realm with
-      // a schema version lower than the one set above
       migrationBlock: { migration, oldSchemaVersion in
-        // We haven’t migrated anything yet, so oldSchemaVersion == 0
         if (oldSchemaVersion < version) {
         }
       })
     
-    // Tell Realm to use this new configuration object for the default Realm
     Realm.Configuration.defaultConfiguration = config
   }
   
