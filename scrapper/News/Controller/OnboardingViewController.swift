b7dbb716248ff106b49b8eed9996179fd726dba9
juyeon <remake867@gmail.com>
2024-07-18 00:15:19 +0900

    온보딩에서 다음 누를 때 버튼 index 꼬이는 현상

--- scrapper/News/Controller/OnboardingViewController.swift
+++ scrapper/News/Controller/OnboardingViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class OnboardingViewController: UIViewController {
   let onboardItems: [OnboardingItem] = [.init(head: "모든 뉴스를 다 볼 수는 없으니까", imageName: "onboarding1-1", foot: "관심있는 키워드를 등록해보세요!"),
                                         .init(head: "그룹으로 묶고, 즐겨찾고", imageName: "onboarding1-2", foot: "나만의 뉴스를 만들어보세요!"),
-                                        .init(head: "요즘 뭐가 이슈인지", imageName: "onboarding1-3", foot: "뒤쳐지지 않게 만들어 드릴게요!")]
+                                        .init(head: "모니터링 해야하는 키워드라며", imageName: "onboarding1-3", foot: "뒤쳐지지 않게 만들어 드릴게요!")
+                                        ]
   @IBOutlet weak var nextBtn: UIButton!
   
   
@@ -20,9 +21,7 @@ class OnboardingViewController: UIViewController {
       pageControl.currentPage = currentPage
       if currentPage == 2 {
         nextBtn.setTitle("지금부터 시작", for: .normal)
-        
         nextBtn.backgroundColor = UIColor(named: "Theme")
-        
       } else {
         nextBtn.setTitle("다음", for: .normal)
         nextBtn.backgroundColor = UIColor(named: "Theme")?.withAlphaComponent(0.5)
@@ -39,11 +38,8 @@ class OnboardingViewController: UIViewController {
         self.present(main, animated: true)
       }
       self.present(payVC, animated: true)
-      
-      
     } else {
-      currentPage += 1
-      let indexPath = IndexPath(item: currentPage, section: 0)
+      let indexPath = IndexPath(item: currentPage + 1, section: 0)
       collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
     }
     
